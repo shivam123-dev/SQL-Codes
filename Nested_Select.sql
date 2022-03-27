fac4601bffb2c8b57fb9c4da6dd975dea7f6c990
shivam123-dev <ss2249@srmist.edu.in>
2022-03-27 16:38:21 +0530

    Nested Select Query in SQL

---
+++ Nested_Select.sql
@@ -0,0 +1,28 @@
+-- Creating table/schema
+create table instructor(
+    ID int(100) primary key, 
+    Name varchar(12) not null,
+    Age int(2),
+    Dept_Name varchar(3),
+    Salary numeric(3,2) not null
+);
+insert into instructor values(1, 'Vaibhav', 12, 'CSE', 100.00);
+insert into instructor values(2, 'Shivam', 22, 'CSE', 130.00);
+insert into instructor values(3, 'Aniket', 32, 'CSE', 120.00);
+insert into instructor values(4, 'Kalyan', 42, 'CSE', 500.00);
+insert into instructor values(5, 'Rahul', 52, 'CSE', 600.00);
+insert into instructor values(6, 'Aniket', 62, 'CSE', 700.00);
+-- selecting distinct values
+select distinct Name from instructor;
+-- selecting all values
+select * from instructor;
+
+
+-- altering instructor table
+alter table instructor drop Name;  -- Drops the column name
+alter table instructor add DOB varchar(5);  -- Adds a new column name
+
+-- nested select 
+select salary from instructor where Name in (select Name from instructor where Name='Aniket');
+
+select * from instructor;
